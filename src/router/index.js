b61996c0d95f5f9d7a5732697830c91599404db3
sjl_git <1075180006@qq.com>
2019-12-23 15:02:37 +0800

    使用element-ui

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+// 导入
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+// 使用
+Vue.use(VueRouter)
+
+// 实例化
+const router = new VueRouter({
+  router: [
+    // 路由规则
+  ]
+})
+// 导出实例
+export default router
