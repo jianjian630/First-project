0312e3c7f449ade6f01c15a1fc26eb20eeb4945c
sjl_git <1075180006@qq.com>
2019-12-23 15:03:34 +0800

    使用element-ui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// router 文件夹 ，默认会加载文件下的index.js
+// index  的含义是索引文件
 
+// @ 是 vue-cli 提供的一个绝对路径的别名
+import router from './router'
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
